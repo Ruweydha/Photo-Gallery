8af42bf5935e22c3a82842c4c0e13a3611bc581c
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-02-26 23:25:13 +0300

    Created tests to save and delete categories and locations

--- images/tests.py
+++ images/tests.py
@@ -1,3 +1,44 @@
+from json import load
+from sre_parse import CATEGORIES
+from unicodedata import name
 from django.test import TestCase
+from .models import Category, Location, Images
 
 # Create your tests here.
+
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.wildlife = Category(name = 'Wildlife')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.wildlife, Category))  
+
+    def test_save_method(self):
+        self.wildlife.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)    
+
+    def test_delete_method(self):
+        self.wildlife.delete_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories)==0)      
+    
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.newYork = Location(location = 'NewYork')   
+
+    def test_instance(self):
+        self.assertTrue(self.newYork, Location)  
+
+    def test_save_location(self):
+        self.newYork.save_location()  
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)  
+
+    def test_delete_method(self):
+        self.newYork.delete_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)==0)         
+
+        
