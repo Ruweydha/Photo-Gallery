b097e952db6233af97282d84e29e383c527fec15
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-02-26 21:51:40 +0300

    Registred the models in the admin page

--- images/admin.py
+++ images/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Images, Category, Location
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Images)
