0d4e18dc97ccb2994d2af64a9e26e8ca5c9131aa
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-02-26 21:49:47 +0300

    Created root page for the site

--- gallery/urls.py
+++ gallery/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, re_path
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
-    re_path(r"^images/" , include('images.urls'))
+    re_path(r"" , include('images.urls'))
 ]
