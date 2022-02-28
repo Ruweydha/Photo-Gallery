942112699f58790d8e3a5468e8e13feaff476b85
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-02-28 12:42:10 +0300

    Viewing images based on the locations available

--- images/urls.py
+++ images/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 urlpatterns = [
     re_path('^$', views.home, name = 'home'),
-    re_path(r'^search/', views.search_category, name ='search')
+    re_path(r'^search/', views.search_category, name ='search'),
+    re_path(r'^location/(.*)/$', views.filter_by_location, name = 'location')
 ]
 
 if settings.DEBUG:
