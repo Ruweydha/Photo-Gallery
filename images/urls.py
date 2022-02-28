c2202af81e2ee7084494192ca16124b05765eed3
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-02-28 12:41:07 +0300

    Added the url for searching based on category and filtering by location

--- images/urls.py
+++ images/urls.py
@@ -1,6 +1,12 @@
 from django.urls import URLPattern, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    re_path('^$', views.welcome, name = 'welcome')
+    re_path('^$', views.home, name = 'home'),
+    re_path(r'^search/', views.search_category, name ='search')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
